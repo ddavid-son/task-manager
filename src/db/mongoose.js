bd85094764c8271ec2755e930d9acd52b3179f1e
david <david@aptenobytes.com>
2022-10-15 18:42:10 +0300

    connction url fix

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,6 +3,6 @@ const mongoose = require("mongoose");
 //const connectionUrl = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager-api";
 
-mongoose.connect(process.env.MONGODB_URL + "/" + databaseName, {
+mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
 });
